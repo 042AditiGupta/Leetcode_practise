e9f389f43c39094368ae189d7c16c73460406c59
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-02-25 16:48:06 +0530

    Time: 27 ms (13.05%) | Memory: 47.8 MB (87.39%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n=piles.length;
+        int max=0;
+        for(int i=0;i<piles.length;i++)
+        {
+            if(piles[i]>max)max=piles[i];
+        }
+        int low=1;
+        int high=max;
+        int sum=0;
+        int k=1;
+        //binary search solution
+        while(low<=high){
+
+            int mid=low+(high-low)/2;
+            sum=0;
+            for(int i=0;i<piles.length;i++)
+            {
+            sum+=Math.ceil((double)piles[i]/mid);
+            }
+            if(sum<=h)
+            {
+                k=mid;
+                high=mid-1;
+              
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return k;
+    }
+}
