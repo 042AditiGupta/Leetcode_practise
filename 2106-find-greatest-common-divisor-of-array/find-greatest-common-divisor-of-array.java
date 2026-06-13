292dc0eef69ef64a131f743c88b46c63cbeb8d57
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-06-13 09:46:51 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (71.79%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,25 +1,17 @@
 class Solution {
     public int findGCD(int[] nums) {
+        int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
-        int max=1;
         for(int i=0;i<nums.length;i++)
         {
-            min=Math.min(min,nums[i]);
             max=Math.max(max,nums[i]);
+            min=Math.min(min,nums[i]);
         }
-        // for(int i=min;i>=1;i--)
-        // {
-        //     if(min % i==0 && max % i==0)
-        //     {
-        //         return i;
-        //     }
-        // }
-        // return 1;
         return gcd(min,max);
     }
     public int gcd(int a,int b)
     {
-        if(b % a==0)return a;
+        if(b%a==0)return a;
         return gcd(b%a,a);
     }
 }
