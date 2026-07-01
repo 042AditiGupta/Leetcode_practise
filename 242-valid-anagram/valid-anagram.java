11b28cbc98e1747ca5a77ab23ea82ea7759d7ee0
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-07-02 01:10:03 +0530

    Time: 20 ms (9.21%) | Memory: 47.2 MB (5.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
+        //store the frequency of s string in the hashmap
+        if(s.length()!=t.length())return false;
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<s.length();i++)
         {
-            return false;
+            if(map.containsKey(s.charAt(i)))
+            {
+                int freq=map.get(s.charAt(i));
+                map.put(s.charAt(i),freq+1);
+            }
+            else
+            {
+                map.put(s.charAt(i),1);
+            }
         }
-        char []s1=s.toCharArray();
-        char []t1=t.toCharArray();
-        Arrays.sort(s1);
-        Arrays.sort(t1);
-        for(int i=0;i<s1.length;i++)
+        //now traverse in the t string
+        for(int i=0;i<t.length();i++)
         {
-            if(s1[i]!=t1[i])return false;
+            if(map.containsKey(t.charAt(i)))
+            {
+                int freq=map.get(t.charAt(i));
+                map.put(t.charAt(i),freq-1);
+            }
+            else
+            {
+                map.put(t.charAt(i),1);
+            }
+        }
+        for(int ele:map.values())
+        {
+            if(ele!=0)return false;
         }
         return true;
+
     }
 }
