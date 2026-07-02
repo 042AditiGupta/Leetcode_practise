929976c416395f102a7e2762834c3fbb22e9cd73
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-07-02 13:52:13 +0530

    Time: 2 ms (98.35%) | Memory: 44.9 MB (57.25%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,23 +1,48 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         //brute force approach
-        ArrayList<Integer>arr=new ArrayList<>();
+        // ArrayList<Integer>arr=new ArrayList<>();
+        // for(int i=0;i<nums1.length;i++)
+        // {
+        //     for(int j=0;j<nums2.length;j++)
+        //     {
+        //         if(nums1[i]==nums2[j])
+        //         {
+        //             if(!arr.contains(nums1[i]))arr.add(nums1[i]);
+        //         }
+        //     }
+        // }
+
+        // int []ans=new int[arr.size()];
+        // for(int k=0;k<arr.size();k++)
+        // {
+        //     ans[k]=arr.get(k);
+        // }
+        // return ans;
+
+        HashSet<Integer>set1=new HashSet<>();
         for(int i=0;i<nums1.length;i++)
         {
-            for(int j=0;j<nums2.length;j++)
+            set1.add(nums1[i]);
+        }
+        HashSet<Integer>ans=new HashSet<>();
+        for(int i=0;i<nums2.length;i++)
+        {
+            if(set1.contains(nums2[i]))
             {
-                if(nums1[i]==nums2[j])
-                {
-                    if(!arr.contains(nums1[i]))arr.add(nums1[i]);
-                }
+                ans.add(nums2[i]);
             }
         }
-
-        int []ans=new int[arr.size()];
-        for(int k=0;k<arr.size();k++)
+        int res[]=new int[ans.size()];
+        int index=0;
+        for(int ele:ans)
         {
-            ans[k]=arr.get(k);
+            res[index++]=ele;
         }
-        return ans;
+        return res;
     }
+
 }
+
+//Time complexity is o(n^2)
+// space complexity is o(n)
