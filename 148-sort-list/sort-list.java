2a657c32685ab4121c30e96668ee19727d88a43e
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-07-09 14:00:53 +0530

    Time: 9 ms (99.18%) | Memory: 59.7 MB (29.89%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,23 +10,53 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ArrayList<Integer>arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null)
+        //divide the linked list into two halves - lefthalf and righthalf by  finding middle of the node
+        ListNode slow=head;
+        if(head==null || head.next==null)return head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null)
         {
-            arr.add(temp.val);
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode firstHalf=head;
+        ListNode secondHalf=slow.next;
+        slow.next=null;
+        firstHalf=sortList(firstHalf);
+        secondHalf=sortList(secondHalf);
+        return mergeList(firstHalf,secondHalf);
+
+    }
+    public ListNode mergeList(ListNode list1,ListNode list2)
+    {
+        ListNode t1=list1;
+        ListNode t2=list2;
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(t1!=null && t2!=null)
+        {
+            if(t1.val<=t2.val)
+            {
+                temp.next=t1;
+                t1=t1.next;
+            }
+            else
+            {
+                temp.next=t2;
+                t2=t2.next;
+            }
             temp=temp.next;
         }
-        Collections.sort(arr);
-        temp=head;
-        for(int i=0;i<arr.size();i++)
+        if(t1!=null)
         {
-            temp.val=arr.get(i);
-            temp=temp.next;
+            temp.next=t1;
+            t1=t1.next;
         }
-        return head;
+        if(t2!=null)
+        {
+            temp.next=t2;
+            t2=t2.next;
+        }
+        return dummy.next;
     }
 }
-
-// O(2*N + N*LogN), we traverse the linked list, store its elements in an array, sort it, and then copy the sorted values back into the original list.
-// O(N) , additional space required to store all the elements of linked list in an array.
