4a943f90b4b2b93672af56daebb3c1498f5f8949
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-07-21 11:37:54 +0530

    Time: 9 ms (45.34%) | Memory: 47 MB (58.73%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,35 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int []ans=new int[2];
+        //2 pointer approach + 2d array
+        int n=nums.length;
+        int [][]numsIndex=new int[n][2];
+        //store the nums element ans index
         for(int i=0;i<nums.length;i++)
         {
-            for(int j=i+1;j<nums.length;j++)
+            numsIndex[i][0]=nums[i];
+            numsIndex[i][1]=i;
+        }
+        //sort array
+        Arrays.sort(numsIndex,(a,b)-> Integer.compare(a[0],b[0]));
+
+        int left=0;
+        int right=n-1;
+        while(left<right)
+        {
+            int sum=numsIndex[left][0]+numsIndex[right][0];
+            if(sum==target)
             {
-                if(nums[i]+nums[j]==target)
-                {
-                    ans[0]=i;
-                    ans[1]=j;
-                }
+               return new int[]{numsIndex[left][1],numsIndex[right][1]};
+            }
+            else if(sum<target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
             }
         }
-        return ans;
+        return new int[]{-1,-1};
     }
 }
