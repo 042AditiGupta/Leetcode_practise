4412e6ca5e85abc6d2a96be22dd336d6d2376a83
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-07-21 23:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (32.92%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void nextPermutation(int[] arr) {
+        //first one is brute force but the time complexity is very high
+        // optimal approach
+        int index=-1;
+        int n=arr.length;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(arr[i]<arr[i+1])
+            {
+                index=i;
+                break;
+            }
+        }
+        if(index==-1)
+        {
+            reverse(arr,0,n-1);
+            return ;
+        }
+        for(int i=n-1;i>index;i--)
+        {
+            if(arr[i]>arr[index])
+            {
+                swap(arr,i,index);
+                break;
+            }
+        }
+        reverse(arr,index+1,n-1);
+    }
+    public void swap(int[]arr,int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public void reverse(int []arr,int i,int j)
+    {
+        while(i<j)
+        {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
