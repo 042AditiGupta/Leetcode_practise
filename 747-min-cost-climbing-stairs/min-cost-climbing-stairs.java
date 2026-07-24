21921c8526ea3945fceed76aeed13c401a690428
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-07-25 00:22:08 +0530

    Time: 1 ms (39.54%) | Memory: 44.8 MB (58.97%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -4,6 +4,7 @@ class Solution {
         int []dp=new int[n];
          dp[0]=cost[0];
          dp[1]=cost[1];
+         //using tabulation
         for(int i=2;i<cost.length;i++)
         {
             dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
