71d22b2c91e2afc4b127afb9c2b18fc79ef7bd38
Aditi Gupta <164906872+042AditiGupta@users.noreply.github.com>
2026-08-04 12:28:54 +0530

    Time: 2 ms (94.02%) | Memory: 72.1 MB (28.80%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -7,15 +7,11 @@ class Solution {
         {
             prefixsum[i]=nums[i-1]*prefixsum[i-1];
         }
-        int []suffixsum=new int[n];
-        suffixsum[n-1]=1;
+        int suf=1;
         for(int i=n-2;i>=0;i--)
         {
-            suffixsum[i]=nums[i+1]*suffixsum[i+1];
-        }
-        for(int i=0;i<n;i++)
-        {
-            prefixsum[i]=prefixsum[i]*suffixsum[i];
+            suf*=nums[i+1];
+            prefixsum[i]*=suf;
         }
         return prefixsum;
     }
